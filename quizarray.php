d0b6328ffef5b2209d74dbb14bce843d25119295
yusrah <ayusirat@gmail.com>
2018-10-19 16:06:38 +0100

    added a new file to loop through three name

---
+++ quizarray.php
@@ -0,0 +1,9 @@
+<?php
+$firstname=array("Nafisat", "Yusirat", "Khairat");
+$lastname=array("Omolara", "Iyabo", "Olaide");
+for ($i=0; $i<$firstname.length ; $i++) { 
+    for ($j=0; $j<$lastname.length ; $j++) { 
+        echo $firstname. " ".$lastname."<br>";
+    }
+}
+?>
